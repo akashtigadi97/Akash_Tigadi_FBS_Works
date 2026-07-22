e736e9c349cb2ea890bc393c78c8ec4c8b6978ee
akashtigadi97 <akashtigadi97@gmail.com>
2026-07-22 21:58:26 +0530

    Add files via upload

---
+++ Core_python/Assignment/Assignment_No_4/Question_12.py
@@ -0,0 +1,18 @@
+# WAP to check if a given number is Armstrong Number
+
+
+n = int(input('Enter a number: '))
+
+temp = n
+digits = len(str(n))
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** digits
+    temp //= 10
+
+if sum == n:
+    print('Armstrong Number')
+else:
+    print('Not Armstrong Number')
